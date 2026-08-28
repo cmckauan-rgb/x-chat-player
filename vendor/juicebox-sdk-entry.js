0648d7999e22f19b602bf2aa8b9fdc0a8cf9b850
cmckauan-rgb <cmckauan@gmail.com>
2026-08-28 20:56:12 -0300

    Load Juicebox WASM directly in browser

--- vendor/juicebox-sdk-entry.js
+++ vendor/juicebox-sdk-entry.js
@@ -1,1 +1,25 @@
-export * from './juicebox-sdk/juicebox-sdk.js';
+import * as juiceboxBg from './juicebox-sdk/juicebox-sdk_bg.js';
+
+const wasmUrl = new URL('./juicebox-sdk/juicebox-sdk_bg.wasm', import.meta.url);
+const response = await fetch(wasmUrl, { cache: 'force-cache' });
+if (!response.ok) {
+  throw new Error(`Falha ao carregar Juicebox WASM: HTTP ${response.status}`);
+}
+
+const imports = { './juicebox-sdk_bg.js': juiceboxBg };
+let instance;
+try {
+  if (WebAssembly.instantiateStreaming) {
+    ({ instance } = await WebAssembly.instantiateStreaming(response.clone(), imports));
+  } else {
+    throw new Error('instantiateStreaming indisponível');
+  }
+} catch (_) {
+  const bytes = await response.arrayBuffer();
+  ({ instance } = await WebAssembly.instantiate(bytes, imports));
+}
+
+juiceboxBg.__wbg_set_wasm(instance.exports);
+
+export const Client = juiceboxBg.Client;
+export const Configuration = juiceboxBg.Configuration;
